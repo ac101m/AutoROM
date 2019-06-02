a6983dd24851a9fb3156fafec3deb9358786b7c7
ac101m <mofty101@gmail.com>
2019-06-02 19:30:01 +0100

    Simplify TUNG directory checking

--- autorom_tung.py
+++ autorom_tung.py
@@ -17,19 +17,19 @@ __TUNG_DIR_VAR = 'tungdir'
 
 # [TODO] Better checks needed here
 def __valid_tung_dir(path):
+    if path == None:
+        return False
     return True
 
 
 # Get tung install directory
 def get_install_dir():
-    if env.get_var(__TUNG_DIR_VAR) != None:
-        return env.get_var(__TUNG_DIR_VAR)
-    else:
-        while True:
-            tungdir = filedialog.askdirectory(title = 'Where is TUNG installed?')
-            if __valid_tung_dir(tungdir):
-                env.set_var(__TUNG_DIR_VAR, tungdir)
-                return tungdir
+    tungDir = env.get_var(__TUNG_DIR_VAR)
+    while not __valid_tung_dir(tungDir):
+        tungDir = filedialog.askdirectory(title = 'Where is TUNG installed?')
+
+    env.set_var(__TUNG_DIR_VAR, tungdir)
+    return tungdir
 
 
 def find_file(name):
