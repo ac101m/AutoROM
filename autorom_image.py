bec3aef71a3a86c050b87d1a62a32b151a3d9880
ac101m <mofty101@gmail.com>
2019-06-05 15:13:25 +0100

    Refactor image and board class from bytes to bits

--- autorom_image.py
+++ autorom_image.py
@@ -6,65 +6,122 @@ import os
 import sys
 
 
-# Class stores a ROM image as a sequence of bytes
-# Also handles input file parsing
+# Exception classes
+class MaxSizeException(Exception): pass
+
+
+# Class stores a memory image as a list of bits and handles file parsing
 class rom_image:
-    data = bytearray()
+    data = []
+    maxSize = None
 
 
-    # Fill with data from arbitrary file
-    def __init__(self, path, romSize):
-        name, ext = os.path.splitext(path)
-        if ext.lower() == '':       self.init_raw(path, romSize)
-        elif ext.lower() == '.txt': self.init_raw(path, romSize)
-        elif ext.lower() == '.hex': self.init_hex(path, romSize)
-        elif ext.lower() == '.b64': self.init_b64(path, romSize)
+    def __init__(self, maxSize = None, path = None):
 
-        # By default, treat the file as raw
+        # If size was specified
+        if maxSize != None:
+            self.set_max_size(maxSize)
+
+        # Load from file if apropriate
+        if path != None:
+            self.load_from_file(path)
+
+
+    # Set the maximum size for this image
+    def set_max_size(self, maxSize):
+        self.maxSize = maxSize
+
+        # No need to do anything
+        if maxSize == None:
+            return
+
+        # If the new max size is smaller than present data
+        if maxSize >= 0:
+            if maxSize < len(self.data):
+                print('WARNING: Image resized from ' + str(len(self.data)) +
+                      ' to ' + str(size) + ' bits. Data may be truncated.')
+                self.data = self.data[:size]
         else:
-            print("WARNING: '" + ext + "' is not a recognised extension, ", end = '')
-            print('file will be treated as raw bytes')
-            self.init_raw(path, romSize)
-
-        # Pad to rom size
-        self.__pad(romSize)
+            print('ERROR: Invalid image size: ' + str(size))
+            sys.exit()
 
 
-    # Print the contents of the image
+    # Append bit to current data
+    def append_bit(self, bit):
+        if len(self.data) + 1 <= self.maxSize:
+            self.data.append(bit)
+        else:
+            raise MaxSizeException(
+                'Failed to append bit to image, maximum size reached.')
+
+
+    # Append byte to current data
+    def append_byte(self, byte):
+        if len(self.data) + 8 <= self.maxSize:
+            for i in range(0, 8):
+                bitMask = 0x80 >> i
+                if byte & bitMask:
+                    self.append_bit(True)
+                else:
+                    self.append_bit(False)
+        else:
+            raise MaxSizeException(
+                'Failed to append byte to image, maximum size reached.')
+
+
+    # Set bit at specific position within image
+    # Set byte at specific position within image
+
+
+    # Print image contents
+    # [TODO] Better implementation with selectable formats might be nice
     def print_all(self):
         print('Image size: ' + str(len(self.data)) + ' Bytes')
         print(self.data)
 
 
-    # Add padding bytes
-    def __pad(self, romSize):
-        while len(self.data) < romSize:
-            self.data.append(0x00)
+#====[FILE LOADING]===========================================================//
+
+    # Master load from file
+    def load_from_file(self, path):
+        try:
+            name, ext = os.path.splitext(path)
+            if ext.lower() in ['.txt', '']:
+                self.init_raw(path)
+            elif ext.lower() in ['.hex']:
+                self.init_hex(path)
+            elif ext.lower() in ['.b64']:
+                self.init_b64(path)
+
+            # By default, treat the file as raw, but print out a message
+            # to let the user know things may be screwy
+            else:
+                print("WARNING: '" + ext + "' is not a recognised extension, " +
+                      'file will be treated as raw bytes')
+                self.init_raw(path, maxBits)
+
+        except MaxSizeException:
+            print('WARNING: File exceeds image size, data truncated.')
+            return
 
 
     # Initialise image directly from file contents
-    def init_raw(self, path, romSize):
+    def init_raw(self, path):
         with open(path, 'rb') as file:
             while True:
                 byte = file.read(1)
-                if byte != b'':
-                    if len(self.data) < romSize:
-                        self.data.append(ord(byte))
-                    else:
-                        print("WARNING: file '" + path + "' ", end = '')
-                        print('exceeds rom capacity, file truncated')
-                        break
-                else:
+                if byte == b'':
                     break
+                self.append_byte(ord(byte))
 
 
     # Initialise from hex file [TODO]
-    def init_hex(self, path, romSize):
+    def init_hex(self, path):
         print('ERROR, hex file parser not yet implemented :(')
         sys.exit()
 
 
     # Initialise from base64 encoded file [TODO]
-    def init_b64(self, path, romSize):
+    def init_b64(self, path):
         print('ERROR, b64 file parser not yet implemented :(')
         sys.exit()
--- autorom_rom.py
+++ autorom_rom.py
@@ -5,6 +5,7 @@
 import os
 import sys
 import json
+import copy
 
 
 # This project
@@ -19,6 +20,20 @@ __BOARD_REGISTRY_PATH = __BOARD_DIR + '/board-registry.json'
 __BOARD_REGISTRY = json.loads(open(__BOARD_REGISTRY_PATH, 'r').read())
 
 
+# Exception classes
+class MaxSizeException(Exception): pass
+
+
+# Get board path from ID
+def get_board_path(boardID):
+    return __BOARD_DIR + '/' + __BOARD_REGISTRY[boardID]['path']
+
+
+# Get json board path from ID
+def get_json_path(boardID):
+    return __BOARD_DIR + '/' + __BOARD_REGISTRY[boardID]['path'] + '.json'
+
+
 # Get list of all board IDs
 def get_board_ids():
     idList = []
@@ -27,14 +42,9 @@ def get_board_ids():
     return idList
 
 
-# Get ROM path from ID
-def get_path(id):
-    return __BOARD_DIR + '/' + __BOARD_REGISTRY[id]['path']
-
-
 # Get ROM capacity from ID
-def get_capacity(id):
-    return __BOARD_REGISTRY[id]['capacity']
+def get_capacity(boardID):
+    return __BOARD_REGISTRY[boardID]['capacity']
 
 
 # Print contents of board registry
@@ -48,63 +58,72 @@ def print_all():
 
 # Class serves as interface for modifying board contents
 class tung_rom_board:
-    data = None
+    jsonPath = None
+    boardPath = None
+    baseJsonData = None
     capacity = None
-    romID = None
+    boardID = None
 
 
     # Initialise
-    def __init__(self, romID):
-        self.romID = romID
-        self.capacity = get_capacity(romID)
+    def __init__(self, boardID):
+        self.boardID = boardID
+        self.capacity = get_capacity(boardID)
 
         # Convert board to JSON and load
-        boardPath = get_path(romID)
-        jsonPath, junk = os.path.splitext(boardPath)
-        jsonPath += '.json'
-        b2j.board_to_json(boardPath, jsonPath)
-        self.data = json.loads(open(jsonPath, 'r').read())
+        self.boardPath = get_board_path(boardID)
+        self.jsonPath = get_json_path(boardID)
+        b2j.board_to_json(self.boardPath, self.jsonPath)
+        self.baseJsonData = json.loads(open(self.jsonPath, 'rb').read())
 
         # Select set_bit method for given ROM
-        setBitMethodName = 'set_bit_json_' + romID
+        setBitMethodName = 'set_bit_' + boardID
         try:
-            setattr(self, 'set_bit_json', getattr(self, setBitMethodName))
+            setattr(self, 'set_bit_boardID', getattr(self, setBitMethodName))
         except:
-            print("ERROR: Failed to find set_bit implementation for " + romID)
-            sys.exit(1)
+            print("ERROR: Couldn't find set_bit implementation for " + boardID)
+            sys.exit()
 
 
-    # Load image
-    def load_image(self, image):
-        self.set_byte(0, 0x81)
+    # Load bits from image
+    def _encode_json_data(self, image):
+        jsonData = copy.deepcopy(self.baseJsonData)
+        try:
+            for i, bit in enumerate(image.data):
+                if bit:
+                    self._set_bit(jsonData, i)
+        except Exception as e:
+            print('WARNING: Image exceeds ROM size, image data truncated.')
+        return jsonData
 
 
-    # Set byte
-    def set_byte(self, index, value):
-        bitMask = 0x80
-        for i in range(0, 8):
-            bitMask = 0x80 >> i
-            if value & bitMask != 0:
-                self.set_bit((index * 8) + i)
-
-
-    # Set bit
-    def set_bit(self, index):
-        if index < self.capacity * 8:
-            self.set_bit_json(index)
+    # Set single bit
+    def _set_bit(self, index, jsonData):
+        if index < self.capacity:
+            self.set_bit_boardID(index)
         else:
-            print('WARNING: Bit ' + str(index) + ' out of range')
+            raise MaxSizeException(
+                'Failed to set bit, bit index out of range.')
 
 
-    # Save ROM to output file
-    def save_tungboard(self, path):
-        with open(path, 'w') as file:
-            file.write("I'm not a tung board!")
+    # Encode board and dump to file
+    def encode(self, outputPath, image):
+
+        # Encode json data with image
+        jsonData = self._encode_json_data(image)
+
+        # Write new json data to temporary
+        tmpJsonPath = get_board_path(self.boardID) + '.tmp.json'
+        with open(tmpJsonPath, 'w') as file:
+            file.write(json.dumps(jsonData))
+
+        # Convert back to tungboard
+        b2j.json_to_board(tmpJsonPath, outputPath)
+        os.remove(tmpJsonPath)
 
 
+#====[BOARD SPECIFIC SET_BIT IMPLEMENTATIONS HERE]============================//
 
-#====[SET BIT IMPLEMENTATIONS HERE]===========================================//
-
-    # Set bit
-    def set_bit_json_matrix_rom_16x16(self, index):
-        print('WARNING: set_bit_json_matrix_rom_16x16 not yet implemented!')
+    # Set bit for 16 by 16 matrix ROM
+    def set_bit_matrix_rom_16x16(self, index, jsonData):
+        print('WARNING: set_bit_matrix_rom_16x16 not yet implemented!')
