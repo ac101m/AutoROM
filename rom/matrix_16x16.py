43044c247dce095e205517ecbb2bf3cf6c9479ba
ac101m <mofty101@gmail.com>
2019-06-14 16:07:35 +0100

    Re-implement matrix-rom setbit using wire class

--- rom/matrix_16x16.py
+++ rom/matrix_16x16.py
@@ -2,55 +2,11 @@
 
 
 # Standard
-import sys
 import numpy as np
 
 
-# Generate wire json data from start
-def gen_wire(start, end):
-
-    # Compute length of vector
-    wireVec = end - start
-    length = np.linalg.norm(wireVec)
-
-    # Compute position
-    position = (start + end) / 2.0
-
-    # v1: Use basic trig to compute angles, janky
-    dx = wireVec[0]
-    dy = wireVec[1]
-    dz = wireVec[2]
-    euler_x = - np.arctan2(dy, np.sqrt(dx**2 + dz**2))
-    euler_y = np.arctan2(dx, dz)
-    euler = np.array([euler_x, euler_y, 0.0]) * (180 / np.pi)
-
-    # Pack up the angles and return
-    return length, position, euler
-
-
-# Generate JSON data from wire
-def gen_wire_json(start, end, inputinput = False):
-    length, position, euler = gen_wire(start, end)
-
-    wire_json = {
-      "$type": "SavedObjects.SavedWire, Assembly-CSharp",
-      "InputInput": inputinput,
-      "length": length,
-      "LocalPosition": {
-        "x": position[0],
-        "y": position[1],
-        "z": position[2]
-      },
-      "LocalEulerAngles": {
-        "x": euler[0],
-        "y": euler[1],
-        "z": euler[2]
-      },
-      "Children": None,
-      "CanHaveChildren": False
-    }
-
-    return wire_json
+# This project
+import tung.wire as TungWire
 
 
 # Set bit for 16 by 16 matrix ROM
@@ -74,4 +30,5 @@ def set_bit(i, jsonData):
     wireEndPos += posDelta * minorIndexVec
 
     # Add wire to json data
-    jsonData['Children'].append(gen_wire_json(wireStartPos, wireEndPos))
+    wire = TungWire.from_position(wireStartPos, wireEndPos)
+    jsonData['Children'].append(wire.as_json_object())
